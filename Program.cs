5e667e63a73d4c09dd6d18221487927433bb0740
Rodolfo045 <47573981+Rodolfo045@users.noreply.github.com>
2019-09-29 19:51:43 -0700

    s
    c

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace SobreCargaFiguras
+{
+
+class Figura
+{
+   private string nombre;
+        private double ladoa;
+        private double ladob;
+
+
+    public string Nombre { get; set; }
+        public double Ladoa { get; set; }
+
+
+        public double Ladob { get; set; }
+
+    }
+
+    class Operaciones
+    {
+        
+
+        public double CalcularArea(double lado1, double lado2)
+        {
+            return (lado1 * lado2) ;
+        }
+
+        public double CalcularArea(double lado1)
+        {
+            return (lado1 * lado1);
+        }
+        
+
+    }
+
+
+    class Principal
+    {
+        Figura fig = new Figura();
+        Operaciones op = new Operaciones();
+
+        public void Calcular()
+        {
+            Console.WriteLine("Ingrese el valor de un lado");
+            fig.Ladoa = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor de un lado");
+            fig.Ladob = Convert.ToDouble(Console.ReadLine());
+            double a;
+
+             a = op.CalcularArea(fig.Ladoa,fig.Ladob);
+             Console.WriteLine("El area de la figura es: "+ a.ToString());
+
+        }
+
+        
+
+
+
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Principal p = new Principal();
+            p.Calcular();
+            
+
+            
+            
+        }
+    }
+}
